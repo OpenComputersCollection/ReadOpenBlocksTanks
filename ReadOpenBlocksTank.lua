4f12fbc73cb83cfdd04eee90ee0f4b0fcc0229b8
Teddy55Codes <teddy@teddy.email>
2023-04-30 21:35:02 +0200

    large numbers are now more easily readable

--- ReadOpenBlocksTank.lua
+++ ReadOpenBlocksTank.lua
@@ -21,6 +21,22 @@ function percentage(value, maxValue)
     return (value / maxValue) * 100
 end
 
+function readable(number)
+    local str_n = tostring(number)
+    local formatted_n = ""
+
+    local count = 0
+    for i = str_n:len(), 1, -1 do
+        count = count + 1
+        formatted_n = str_n:sub(i, i) .. formatted_n
+
+        if count % 3 == 0 and i > 1 then
+            formatted_n = "'" .. formatted_n
+        end
+    end
+    return formatted_n
+end
+
 print("Enter where the direction the redstone signal should be read from.\nbottom=0\ntop=1\nback=2\nfront=3\nright=4\nleft=5\n(1,2,3,4,5): ")
 local side = tonumber(term.read())
 print("Enter the with of your tank:")
@@ -37,10 +53,10 @@ if component.isAvailable("redstone") then
         local totalCapacityUsed = signalStrength * width * length
         local totalCapacity = tankCapacity * width * length * height
         term.clear()
-        print(totalCapacityUsed .. "/" .. totalCapacity)
+        print(readable(string.format("%.0f", totalCapacityUsed)) .. "/" .. readable(totalCapacity))
         local prtg = percentage(totalCapacityUsed, totalCapacity)
         print(progressbar(prtg))
-        print(prtg .. "%")
+        print(string.format("%.2f", prtg) .. "%")
         os.sleep(1)
     end
 
